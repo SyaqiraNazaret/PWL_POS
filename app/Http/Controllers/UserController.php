9bd50e676d909886274178f67f61f3c27eb82115
SyaqiraNazaret <snazaretna@gmail.com>
2025-03-08 16:35:39 +0700

    Update 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -54,9 +54,7 @@ class UserController extends Controller
         // $user = UserModel::firstWhere('level_id',1);
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     // coba akses model UserModel
         // $user = UserModel::all(); // ambil semua data dari tabel m_user
